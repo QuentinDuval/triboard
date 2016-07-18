25334894ae510e8bbe4471c470df4a73193ac9d6
QuentinDuval <senmenty@gmail.com>
2016-07-18 14:46:34 +0200

    The board

--- src/cljs/triboard/core.cljs
+++ src/cljs/triboard/core.cljs
@@ -3,18 +3,55 @@
 
 (enable-console-print!)
 
-(def ^:const board-height 11)
-(def ^:const board-width 17)
+;; -----------------------------------------
 
-(defn init-state
+(def board-width 16)
+(def board-height 11)
+(def init-block-count 12) ;; Init blocks for red, blue, green, gray
+
+(def scale-factor 100)
+
+;; -----------------------------------------
+
+(defn new-board
+  "Create a new board"
   []
-  [])
+  (vec
+    (repeat board-width
+      (vec (repeat board-height :empty))
+      )))
+
+;; -----------------------------------------
 
 (defonce app-state
-  (atom {:text "Hello Chestnut!"}))
+  (atom
+    {:board (new-board)
+     }))
+
+(defn empty-cell
+  "Draws an empty cell"
+  [x y]
+  [:rect {:width 0.9
+          :height 0.9
+          :x (+ 0.05 x)
+          :y (+ 0.05 y)
+          :fill "lightgrey"
+          }])
 
 (defn greeting []
-  [:h1 (:text @app-state)])
+  [:h1 "Triboard"
+   (into
+     [:svg#board
+      {:view-box (str "0 0 " board-width " " board-height)}]
+     (for [x (range board-width)
+           y (range board-height)]
+       ^{:key [x y]}
+       (case (get-in @app-state [:board x y])
+         :empty  [empty-cell x y])
+       ))
+   ])
+
+;; -----------------------------------------
 
 (reagent/render [greeting]
   (js/document.getElementById "app"))
