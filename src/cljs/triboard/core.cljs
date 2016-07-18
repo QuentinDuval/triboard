85a0e761fad89fc9ed0dafef075a9a44d664ef18
QuentinDuval <senmenty@gmail.com>
2016-07-18 15:58:50 +0200

    Init board

--- src/cljs/triboard/core.cljs
+++ src/cljs/triboard/core.cljs
@@ -8,6 +8,7 @@
 (def board-width 16)
 (def board-height 11)
 (def init-block-count 12) ;; Init blocks for red, blue, green, gray
+(def players [:blue :red :green])
 
 ;; -----------------------------------------
 
@@ -35,22 +36,28 @@
   "Create random initial positions for the players"
   []
   (draw-slices init-block-count
-    [:blue :red :green :gray] (shuffle all-positions)))
+    (conj players :gray)
+    (shuffle all-positions)))
 
-(defn new-board
-  "Create a new board"
-  []
+(defn new-board []
   (reduce (fn [r [color [x y]]]
             (prn [x y])
             (assoc-in r [x y] color))
     empty-board (init-positions)))
 
+(defn new-game []
+  {:board (new-board)
+   :turn 0
+   ;; From the turn you can deduce the number of turn left
+   ;; There is an invariant holding: total score = 12 * 3 + turn
+   :scores
+   {:blue 12
+    :red 12
+    :green 12}})
+
 ;; -----------------------------------------
 
-(defonce app-state
-  (atom
-    {:board (new-board)
-     }))
+(defonce app-state (atom (new-game)))
 
 ;; -----------------------------------------
 
