429c7afa635c73b88bb6b471b143723fd29985b3
Duval Quentin <senmenty@gmail.com>
2017-01-07 15:30:46 +0100

    simplify score spec

--- src/cljs/triboard/logic/scores.cljs
+++ src/cljs/triboard/logic/scores.cljs
@@ -1,7 +1,6 @@
 (ns triboard.logic.scores
   (:require
     [cljs.spec :as s :include-macros true]
-    [cljs.spec.impl.gen :as gen]
     [triboard.logic.constants :as cst]
     [triboard.logic.move :as move]
     ))
@@ -12,15 +11,11 @@
 ;; -----------------------------------------
 
 (s/def ::pos-int (s/and integer? #(<= 0 %)))
-(s/def ::blue ::pos-int)
-(s/def ::red ::pos-int)
-(s/def ::green ::pos-int)
-(s/def ::scores (s/keys :req-un [::blue ::red ::green]))
+(s/def ::scores (s/map-of ::cst/player ::pos-int))
 
 (s/fdef update-scores
   :args (s/tuple ::scores ::move/conversion)
-  ;; :ret ::scores ;;TODO - There could be violation without more constraints on inputs
-  )
+  :ret (s/map-of ::cst/player integer?))                    ;; Hard to ensure more
 
 (def initial-scores
   {:blue cst/init-block-count
@@ -30,9 +25,8 @@
 (defn update-scores
   "Update the scoring based on the provided move"
   [scores move]
-  {:pre [(move/conversion? move)]}
   (let [delta (count (:taken move))]
     (-> scores
       (update (:winner move) + delta)
       (update (:looser move) - delta)
-      )))
+      (dissoc :empty))))
