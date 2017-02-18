580e31d56a2900d930e088aa2d72c278cd27503e
Duval Quentin <senmenty@gmail.com>
2017-02-18 17:44:31 +0100

    view refactoring

--- src/cljs/triboard/view/board.cljs
+++ src/cljs/triboard/view/board.cljs
@@ -7,8 +7,12 @@
     ))
 
 
-(defn rect-cell
-  [x y player options]
+;; ----------------------------------------------------------------------------
+;; Private
+;; ----------------------------------------------------------------------------
+
+(defn- rect-cell
+  [[x y] player options]
   [:rect.cell
    (merge
      {:class (str "cell--" (name player))
@@ -16,21 +20,28 @@
       :y (+ 0.05 y) :height 0.9}
      options)])
 
-(defn empty-cell
-  [x y cb player]
-  (rect-cell x y player
-    {:on-click #(cb/on-player-move cb x y)}
-    ))
+(defn- empty-cell
+  [[x y :as position] cb player]
+  (rect-cell position player
+    {:on-click #(cb/on-player-move cb x y)}))
+
+(defn- empty-svg-board []
+  [:svg.board
+   {:view-box (str "0 0 " cst/board-width " " cst/board-height)
+    :style {:max-height (str (vutils/max-board-height) "px")}}])
+
+
+;; ----------------------------------------------------------------------------
+;; Public
+;; ----------------------------------------------------------------------------
 
 (defn render-board
   [board suggestions cb]
   (into
-    [:svg.board
-     {:view-box (str "0 0 " cst/board-width " " cst/board-height)
-      :style {:max-height (str (vutils/max-board-height) "px")}}]
-    (for [[[x y] cell] (model/to-iterable board)]
-      ^{:key [x y]}
+    (empty-svg-board)
+    (for [[position cell] (model/to-iterable board)]
+      ^{:key position}
       (if (= :empty cell)
-        [empty-cell x y cb (if (suggestions [x y]) :help :empty)]
-        [rect-cell x y cell])
+        [empty-cell position cb (if (suggestions position) :help :empty)]
+        [rect-cell position cell])
       )))
