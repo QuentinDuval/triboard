011eb748c2ec7aa406173e069f2e8c42767e1347
Duval Quentin <senmenty@gmail.com>
2017-02-18 17:22:12 +0100

    refactor gui

--- src/cljs/triboard/view/panel.cljs
+++ src/cljs/triboard/view/panel.cljs
@@ -11,13 +11,21 @@
 (s/fdef show-scores
   :args (s/tuple #(every? % cst/players)))
 
+(defn- player->css-style
+  [player]
+  {:class (str "score--" (name player))})
+
+(defn- player->score-text
+  [player score]
+  (str (str/capitalize (name player)) " - " score))
+
 (defn- show-scores
-  [scores player]
+  [scores highlight?]
   (for [p cst/players]
     ^{:key p}
-    [(if (= player p) :div.score--is-current :div.score)
-     {:class (str "score--" (name p))}
-     (str (str/capitalize (name p)) " - " (get scores p))]
+    [(if (highlight? p) :div.score--is-current :div.score)
+     (player->css-style p)
+     (player->score-text p (get scores p))]
     ))
 
 (defn- top-panel-button
@@ -25,11 +33,14 @@
   [:button.help-button {:on-click on-click} txt])
 
 (defn show-top-panel
-  [scores player cb]
+  "Show the top panel of the game that contains
+   * The player scores
+   * The main commands"
+  [scores current-player cb]
   [:div.scores
    [top-panel-button #(cb/on-new-game cb) vutils/star]
    [top-panel-button #(cb/on-toogle-help cb) "?"]
-   (show-scores scores player)
+   (show-scores scores #(= % current-player))
    [top-panel-button #(cb/on-restart cb) vutils/circle-arrow]
    [top-panel-button #(cb/on-undo cb) vutils/back-arrow]
    ])
