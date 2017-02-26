9b9b413fde354760b0c4d81a207030609ffde49e
Duval Quentin <senmenty@gmail.com>
2017-02-26 16:13:25 +0100

    clean up ai code

--- src/cljs/triboard/ai/ai.cljs
+++ src/cljs/triboard/ai/ai.cljs
@@ -3,7 +3,6 @@
     [cljs.spec :as s :include-macros true]
     [triboard.ai.scoring :as scoring]
     [triboard.logic.game :as game]
-    [triboard.logic.player :as player]
     [triboard.logic.scores :as scores]
     [triboard.logic.turn :as turn]
     ))
@@ -16,68 +15,10 @@
 (defn- make-ai
   [board player]
   {:player player
-   :other-players (remove #{player} player/all)
-   :cell-weights (scoring/get-weighting board)
+   :cell-weights (scoring/get-weighting board)              ;; TODO
    })
 
-(defn- score-move
-  "Compute the strength of a move, based on the converted cells"
-  [{:keys [player cell-weights]} transition]
-  (reduce                                                   ;; TODO - Factor in move
-    #(scores/update-scores-with %1 %2 cell-weights)
-    scores/null-scores
-    transition))
-
-;; -----------------------------------------
-;; Attempts game - tree (later minimax decoupled)
-;; -----------------------------------------
-
-(defn build-game-tree
-  [game depth]
-  (if (= 0 depth)
-    game
-    (let [transitions (turn/player-transitions (game/current-turn game))
-          next-games (map #(game/play-move game (first %)) transitions)]
-      (map vector
-        transitions
-        (map #(build-game-tree % (dec depth)) next-games)))
-    ))
-
-(defn test-build-game-tree
-  []
-  (let [init-game (game/new-game)]
-    (build-game-tree init-game 1)))
-
-;; -----------------------------------------
-;; Attempts minimax
-;; -----------------------------------------
-
-;; TODO - How to add the custom scoring? How to keep the game? (use keys)
-;; TODO - Use mapmin from http://worrydream.com/refs/Hughes-WhyFunctionalProgrammingMatters.pdf
-;; TODO - Sort by scores as well
-
-
-(defn contains-lower-value?
-  [coll pot]
-  (cond
-    (empty? coll) false
-    (<= (first coll) pot) true
-    :else (recur (rest coll) pot)
-    ))
-
-(defn map-min-impl
-  [max-min-val coll]
-  (cond
-    (empty? coll) max-min-val
-    (contains-lower-value? (first coll) max-min-val) max-min-val ;; Will depend on player
-    :else (recur
-            (max max-min-val (apply min (first coll)))      ;; Will depend on player
-            (rest coll))
-    ))
-
-(defn map-min
-  [coll]
-  (map-min-impl (min (first coll)) (rest coll)))
+(defn- max-by [key-fn coll] (apply max-key key-fn coll))
 
 ;; -----------------------------------------
 
@@ -110,45 +51,6 @@
     ))
 
 ;; -----------------------------------------
-
-(defn test-on-game
-  [g]
-  (let [turn (game/current-turn g)
-        ai (make-ai (:board turn) (:player turn))]
-    (tree-score ai (game/current-turn g) 2)))
-
-(defn test-score-tree [] (test-on-game (game/new-game)))
-
-(defn benchmark []
-  (time (dotimes [i 10]
-          (time (test-score-tree))
-          )))
-
-;; -----------------------------------------
-
-(defn- worst-possible-score-from
-  [{:keys [player other-players] :as ai} moves]
-  (transduce
-    (comp
-      (mapcat #(get moves %))
-      (map #(score-move ai (second %)))
-      (map #(get % player)))
-    min
-    other-players))
-
-(defn- move-best-outcome
-  [ai game [coord transition]]
-  (let [new-game (game/play-move game coord)
-        move-diff (get (score-move ai transition) (:player ai))
-        new-moves (:transitions (game/current-turn new-game))
-        next-diff (worst-possible-score-from ai new-moves)]
-    {:scoring (+ move-diff next-diff)
-     :game new-game}))
-
-(defn- max-by [key-fn coll] (apply max-key key-fn coll))
-
-
-;; -----------------------------------------
 ;; Public API
 ;; -----------------------------------------
 
@@ -171,16 +73,20 @@
           (turn/player-transitions turn)))
       )))
 
-#_(defn- play-best-move
-    "[SIMPLISTIC] Play the best move for a player based on:
-     * The immediate gain
-     * The worse immediate lost afterwards"
-    [game]
-    (let [turn (game/current-turn game)
-          ai (make-ai (:board turn) (:player turn))]
-      (:game
-        (max-by :scoring
-          (map
-            #(move-best-outcome ai game %)
-            (turn/player-transitions turn)))
-        )))
+
+;; -----------------------------------------
+;; TESTS
+;; -----------------------------------------
+
+(defn test-on-game
+  [g]
+  (let [turn (game/current-turn g)
+        ai (make-ai (:board turn) (:player turn))]
+    (tree-score ai (game/current-turn g) 2)))
+
+(defn test-score-tree [] (test-on-game (game/new-game)))
+
+(defn benchmark []
+  (time (dotimes [i 10]
+          (time (test-score-tree))
+          )))
