70ec2bd76bd83cd543b0df82f109cd5ae89733ed
Duval Quentin <senmenty@gmail.com>
2017-03-04 17:32:13 +0100

    renaming to jump

--- src/cljs/triboard/logic/transition.cljs
+++ src/cljs/triboard/logic/transition.cljs
@@ -11,16 +11,16 @@
 ;; Public Types
 ;; -----------------------------------------
 
-(s/def ::point ::board/coord)
+(s/def ::destination ::board/coord)
 (s/def ::taken (s/coll-of ::board/coord))
 (s/def ::winner ::player/player)
 (s/def ::looser ::player/playable-cell)
-(s/def ::conversion (s/keys :req-un [::point ::taken ::winner ::looser]))
-(s/def ::transition (s/every ::conversion))
+(s/def ::jump (s/keys :req-un [::destination ::taken ::winner ::looser]))
+(s/def ::transition (s/every ::jump))
 
 (s/def ::transitions
   (s/map-of ::player/player
-    (s/map-of ::board/coord ::transition)))
+    (s/map-of ::destination ::transition)))
 
 
 ;; -----------------------------------------
@@ -39,18 +39,18 @@
 
 (defn- available-cells-by-dir
   "Indicates the convertible cells when clicking at [x y]"
-  [board [x-init y-init :as init-coord] [dx dy]]
+  [board [x-init y-init :as destination] [dx dy]]
   (loop [x (+ x-init dx)
          y (+ y-init dy)
          looser nil
          taken []]
     (let [cell (aboard-cell-at board x y)]
       (cond
-        (is-not-convertible? cell) nil                      ;; No move: reached end and only 1 type of cell
-        (and looser (not= looser cell)) {:winner cell       ;; Who wins the cells
-                                         :looser looser     ;; Who looses the cells
-                                         :point init-coord  ;; The move performed
-                                         :taken taken}      ;; The cells taken
+        (is-not-convertible? cell) nil                            ;; No move: reached end and only 1 type of cell
+        (and looser (not= looser cell)) {:winner cell             ;; Who wins the cells
+                                         :looser looser           ;; Who looses the cells
+                                         :destination destination ;; The move performed
+                                         :taken taken}            ;; The cells taken
         :else (recur
                 (+ x dx)
                 (+ y dy)
@@ -58,7 +58,7 @@
                 (conj taken [x y])))
       )))
 
-(defn- available-conversions-at
+(defn- available-jumps-at
   "Provides the list of moves that can be done from a cell"
   [board point]
   (eduction
@@ -68,21 +68,21 @@
 (defn- empty-cell-conversion
   "Create a move to take an empty cell"
   [player point]
-  {:point point
+  {:destination point
    :winner player
    :looser :empty
    :taken [point]})
 
-(defn- apply-conversion
+(defn- apply-jump
   "Apply a move onto the board, yielding a new board"
   [board {:keys [winner taken]}]
   (reduce #(board/convert-cell %1 %2 winner) board taken))
 
-(defn conversions->transition
-  [conversions]
-  (if-let [{:keys [winner point]} (first conversions)]
-    (conj conversions (empty-cell-conversion winner point))
-    conversions))
+(defn add-destination
+  [jumps]
+  (if-let [{:keys [winner destination]} (first jumps)]
+    (conj jumps (empty-cell-conversion winner destination))
+    jumps))
 
 (defn- map-transition-tree
   [xf game-tree]
@@ -100,15 +100,15 @@
   [board]
   (let [aboard (board/board->array board)]
     (map-transition-tree
-      conversions->transition
+      add-destination
       (transduce
-        (mapcat #(available-conversions-at aboard %))
-        (algo/group-by-reducer :winner :point)
+        (mapcat #(available-jumps-at aboard %))
+        (algo/group-by-reducer :winner :destination)
         (board/empty-cells board)))))
 
 (defn apply-transition
   [board transition]
-  (reduce apply-conversion board transition))
+  (reduce apply-jump board transition))
 
 ;; -----------------------------------------
 ;; TESTS
