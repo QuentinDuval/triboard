79e7345961d2452659c6bda3dab18b6fd67d1780
Duval Quentin <senmenty@gmail.com>
2017-03-18 15:14:50 +0100

    fix board

--- src/cljs/triboard/view/board.cljs
+++ src/cljs/triboard/view/board.cljs
@@ -1,15 +1,13 @@
 (ns triboard.view.board
   (:require
     [triboard.logic.board :as board]
-    [triboard.view.interactions :as cb]
+    [triboard.view.interactions :as i]
     [triboard.view.utils :as vutils]))
 
 
 ;; ----------------------------------------------------------------------------
 ;; Private
 ;; ----------------------------------------------------------------------------
-
-;; TODO - Square SVG Board utils
 
 (def relative-size 0.9)
 (def border-size (/ (- 1 relative-size) 2))
@@ -24,9 +22,9 @@
      options)])
 
 (defn- empty-cell
-  [[x y :as position] cb player]
+  [[x y :as position] interactions player]
   (rect-cell position player
-    {:on-click #(cb/on-player-move cb x y)}))
+    {:on-click #(i/on-player-move interactions x y)}))
 
 (defn- empty-svg-board []
   [:svg.board
@@ -39,14 +37,13 @@
 ;; ----------------------------------------------------------------------------
 
 (defn render-board
-  [board suggestions cb]
+  [board suggestions interactions]
   (into
     (empty-svg-board)
     (for [[position cell] (board/to-seq board)]
       ^{:key position}
       (if (= :none cell)
-        [empty-cell position cb
-         (if (and suggestions (suggestions position))       ;; Avoids crash !
-           :help :none)]
+        [empty-cell position interactions
+         (if (suggestions position) :help :none)]
         [rect-cell position cell])
       )))
